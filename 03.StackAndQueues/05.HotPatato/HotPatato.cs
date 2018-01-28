0e57bfd1ce900cc230e291be84b17ddebf4c571c
vsaraminev <v.saraminev@abv.bg>
2018-01-28 13:04:58 +0200

    Add StackAndQueues lab

---
+++ 03.StackAndQueues/05.HotPatato/HotPatato.cs
@@ -0,0 +1,30 @@
+﻿namespace HotPatato
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class HotPatato
+    {
+        public static void Main()
+        {
+
+            var people = Console.ReadLine().Split(' ');
+
+            var rotations = int.Parse(Console.ReadLine());
+
+            var queue = new Queue<string>(people);
+
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i < rotations; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
