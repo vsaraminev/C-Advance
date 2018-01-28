6b26140445e5de4fbdeddc62696331122abbb367
vsaraminev <v.saraminev@abv.bg>
2018-01-28 13:06:05 +0200

    Rename Root Folder

---
+++ 03.StackAndQueuesLab/01.ReverseStrings/ReverseStrings.cs
@@ -0,0 +1,29 @@
+﻿namespace ReverseStrings
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class ReverseStrings
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var characters = new Stack<char>();
+
+            //var stack = new Stack<char>(input.ToCharArray());
+
+            foreach (var ch in input)
+            {
+                characters.Push(ch);
+            }
+
+            while (characters.Count > 0)
+            {
+                Console.Write(characters.Pop());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 03.StackAndQueuesLab/03.DecToBinConverter/DecToBinConverter.cs
@@ -0,0 +1,34 @@
+﻿namespace DecToBinConverter
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class DecToBinConverter
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                Console.WriteLine(number);
+                return;
+            }
+
+            var stack = new Stack<int>();
+
+            while (number > 0)
+            {
+                stack.Push(number % 2);
+                number /= 2;
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
